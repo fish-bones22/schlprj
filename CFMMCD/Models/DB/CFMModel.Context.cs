9f28114b0f1211e826926b55eda84eee4f9002cc
JDsaur <jdreyes662@gmail.com>
2016-12-23 22:46:45 +0800

    Change the table for the Price Tier

--- CFMMCD/Models/DB/CFMModel.Context.cs
+++ CFMMCD/Models/DB/CFMModel.Context.cs
@@ -27,10 +27,17 @@ namespace CFMMCD.Models.DB
     
         public virtual DbSet<Account> Accounts { get; set; }
         public virtual DbSet<Audit_Log> Audit_Log { get; set; }
+        public virtual DbSet<Breakfast_Price_Tier> Breakfast_Price_Tier { get; set; }
         public virtual DbSet<CSHMIMP0> CSHMIMP0 { get; set; }
         public virtual DbSet<CSHMIMP0_NP6> CSHMIMP0_NP6 { get; set; }
+        public virtual DbSet<Dessert_Price_Tier> Dessert_Price_Tier { get; set; }
         public virtual DbSet<INVRIMP0> INVRIMP0 { get; set; }
-        public virtual DbSet<Price_Tier> Price_Tier { get; set; }
+        public virtual DbSet<McCafe_Bistro_Price_Tier> McCafe_Bistro_Price_Tier { get; set; }
+        public virtual DbSet<McCafe_Level_2_Price_Tier> McCafe_Level_2_Price_Tier { get; set; }
+        public virtual DbSet<McCafe_Level_3_Price_Tier> McCafe_Level_3_Price_Tier { get; set; }
+        public virtual DbSet<MDS_Price_Tier> MDS_Price_Tier { get; set; }
+        public virtual DbSet<Project_Gold_Price_Tier> Project_Gold_Price_Tier { get; set; }
+        public virtual DbSet<Regular_Price_Tier> Regular_Price_Tier { get; set; }
         public virtual DbSet<Store_Profile> Store_Profile { get; set; }
     }
 }
