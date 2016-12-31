6bf5132ca4d78d1f690f1d0c59b5265699937da9
edward487 <drawdej963@gmail.com>
2016-12-31 00:23:42 -0800

    added report generation view

---
+++ CFMMCD/Controllers/ReportGenerationController .cs
@@ -0,0 +1,19 @@
+﻿using CFMMCD.Models.EntityManager;
+using CFMMCD.Models.ViewModel;
+using CFMMCD.Sessions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CFMMCD.Controllers
+{
+    public class ReportGenerationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
