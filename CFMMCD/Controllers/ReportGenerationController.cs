67d30e3bc16df30f32c5c4b4a5f1633fec5de742
fish-bones22 <samuelquinto22@gmail.com>
2017-01-04 13:28:17 +0800

    fixed merge conflicts

---
+++ CFMMCD/Controllers/ReportGenerationController.cs
@@ -0,0 +1,25 @@
+﻿using CFMMCD.Models.EntityManager;
+using CFMMCD.Models.ViewModel;
+using CFMMCD.Sessions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CFMMCD.Controllers
+{
+    public class ReportGenerationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ExportPDF()
+        {
+            var model = 0;//new GeneratePDF();
+            //Code to get content
+            return new Rotativa.ActionAsPdf("ReportGeneration", model) { FileName = "ReportGeneration.pdf" };
+        }
+    }
+}
